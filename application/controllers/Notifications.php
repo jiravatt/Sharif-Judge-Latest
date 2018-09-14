0dcfa55c5a7cf98158e18f72a04e9b2c4db207c5
Jiravatt Rewrujirek <jiravatt@users.noreply.github.com>
2018-09-14 14:55:26 +0700

    Add redirect to install

--- application/controllers/Notifications.php
+++ application/controllers/Notifications.php
@@ -18,6 +18,8 @@ class Notifications extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		if ( ! $this->db->table_exists('sessions'))
+			redirect('install');
 		if ( ! $this->session->userdata('logged_in')) // if not logged in
 			redirect('login');
 		$this->load->model('notifications_model');
