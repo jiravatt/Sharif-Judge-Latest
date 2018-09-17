fc5c42bd491eb6453274ea8bae14e18ed18a5d84
Jiravatt Rewrujirek <jiravatt@users.noreply.github.com>
2018-09-17 21:33:16 +0700

    Add mainprog.* to zip + Retain .md, .pdf when reupload

--- application/controllers/Assignments.php
+++ application/controllers/Assignments.php
@@ -154,6 +154,19 @@ class Assignments extends CI_Controller
 			$path = "$root_path/p{$i}/tester.cpp";
 			if (file_exists($path))
 				$this->zip->add_data("p{$i}/tester.cpp", file_get_contents($path));
+			
+			// Add mainprog.* into a zip file
+			$path = "$root_path/p{$i}/mainprog.cpp"; 
+			if (file_exists($path))
+				$this->zip->add_data("p{$i}/mainprog.cpp", file_get_contents($path));
+			
+			$path = "$root_path/p{$i}/mainprog.c";
+			if (file_exists($path))
+				$this->zip->add_data("p{$i}/mainprog.c", file_get_contents($path));
+			
+			$path = "$root_path/p{$i}/mainprog.py";
+			if (file_exists($path))
+				$this->zip->add_data("p{$i}/mainprog.py", file_get_contents($path));
 
 			$pdf_files = glob("$root_path/p{$i}/*.pdf");
 			if ($pdf_files)
@@ -503,12 +516,12 @@ class Assignments extends CI_Controller
 			{
 				// Remove previous test cases and descriptions
 				shell_exec("cd $assignment_dir;"
-					." rm -rf */in; rm -rf */out; rm -f */tester.cpp; rm -f */tester.executable;"
-					." rm -f */desc.html; rm -f */desc.md; rm -f */*.pdf;");
+					." rm -rf */in; rm -rf */out; rm -f */tester.cpp; rm -f */tester.executable;");
+//					." rm -f */desc.html; rm -f */desc.md; rm -f */*.pdf;");
 				if (glob("$tmp_dir/*.pdf"))
 					shell_exec("cd $assignment_dir; rm -f *.pdf");
 				// Copy new test cases from temp dir
-				shell_exec("cd $assignments_root; cp -R $tmp_dir_name/* assignment_{$the_id};");
+				shell_exec("cd $assignments_root; \\cp -Rf $tmp_dir_name/* assignment_{$the_id};");
 				$this->messages[] = array(
 					'type' => 'success',
 					'text' => 'Tests (zip file) extracted successfully.'
